8f53c2fa250ee061c229d9cea76e346837da784c
Gencehan <gencehangurcam@gmail.com>
2021-11-08 10:39:44 +0100

    project init

---
+++ main.js
@@ -0,0 +1,10 @@
+let screen = document.querySelector("screen")
+let button = document.querySelector("button")
+let operation = document.querySelector("operation")
+let result = document.querySelector("result")
+
+button = ""
+
+button.addEventListener('click', () => {
+    console.log('hey');
+})
